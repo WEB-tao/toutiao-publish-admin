bf40fccefaae07a65fa8d1e53cdd8e4cf502101d
马松涛 <songtao_1996@163.com>
2020-12-12 13:35:11 +0800

    项目初始化

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
+
+######## 头条后台管理系统 新增目录结构
+
+# src/api 目录
+###     ·储存接口的模块
+
+# src/utils 目录
+###     ·储存一些工具模块
+
+# src/styles 目录
+###     ·index.less文件, 储存全局样式
+###     ·在main.js 中加载全局的样式
+
+
+
+
+
 # vue_toutiao_admin
 
 ## Project setup
--- src/main.js
+++ src/main.js
@@ -1,9 +1,20 @@
+/*
+* 项目的启动入口
+*/
+
+// 加载以下模块组件
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+// 加载全局样式文件
+import './styles/index.less'
+
 Vue.config.productionTip = false
 
+// 创建 vue 根实例
+// 把 router 配置到根实例中
+// 通过 render 方法 把APP 根组件渲染到 #app 入口节点
 new Vue({
   router,
   render: h => h(App)
